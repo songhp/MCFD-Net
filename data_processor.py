2c6252a67269b79a9eb7b84ea43b127962c025a5
gongjy <2474590974@qq.com>
2023-12-15 11:36:35 +0800

    first update

--- data_processor.py
+++ data_processor.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import torchvision
 import torch
 from torch.utils.data import DataLoader
@@ -31,11 +30,11 @@ def data_loader(args, root='./', is_test=False):
         torchvision.transforms.ToTensor(),
     ])
 
-    trn_dataset = torchvision.datasets.ImageFolder(root + 'dataset/train_80k', transform=trn_transforms)
+    trn_dataset = torchvision.datasets.ImageFolder(root + 'dataset/train_40k', transform=trn_transforms)
     test_bsds = torchvision.datasets.ImageFolder(root + 'dataset/BSDS200', transform=test_bsds_transforms)
     test_set5 = torchvision.datasets.ImageFolder(root + 'dataset/set5', transform=test_set5_transforms)
     test_set14 = torchvision.datasets.ImageFolder(root + 'dataset/set14', transform=test_set14_transforms)
-    compare = torchvision.datasets.ImageFolder(root + 'dataset/set14', transform=test_compare_transforms)
+    compare = torchvision.datasets.ImageFolder(root + 'dataset/compare', transform=test_compare_transforms)
 
     test_loader_bsds = DataLoader(test_bsds, batch_size=1, shuffle=True, **kwopt, drop_last=False)
     test_loader_set5 = DataLoader(test_set5, batch_size=1, shuffle=True, **kwopt, drop_last=False)
@@ -49,55 +48,3 @@ def data_loader(args, root='./', is_test=False):
                             drop_last=False)
 
     return trn_loader, test_loader_bsds, test_loader_set5, test_loader_set14
-=======
-import torchvision
-import torch
-from torch.utils.data import DataLoader
-
-
-def data_loader(args, root='./', is_test=False):
-    kwopt = {'num_workers': 8, 'pin_memory': True}
-    w_size, h_size = int(16*6), int(16*6)
-    trn_transforms = torchvision.transforms.Compose([
-        torchvision.transforms.Resize((128, 128)),
-        torchvision.transforms.RandomCrop(args.image_size),
-        torchvision.transforms.RandomHorizontalFlip(),
-        torchvision.transforms.ToTensor(),
-    ])
-
-    test_set5_transforms = torchvision.transforms.Compose([
-        torchvision.transforms.Resize((w_size, h_size)),
-        torchvision.transforms.ToTensor(),
-    ])
-    test_set14_transforms = torchvision.transforms.Compose([
-        torchvision.transforms.Resize((w_size, h_size)),
-        torchvision.transforms.ToTensor(),
-    ])
-    test_bsds_transforms = torchvision.transforms.Compose([
-        torchvision.transforms.Resize((w_size, h_size)),
-        torchvision.transforms.ToTensor(),
-    ])
-    test_compare_transforms = torchvision.transforms.Compose([
-        torchvision.transforms.Resize((w_size, h_size)),
-        torchvision.transforms.ToTensor(),
-    ])
-
-    trn_dataset = torchvision.datasets.ImageFolder(root + 'dataset/BSDS200', transform=trn_transforms)
-    test_bsds = torchvision.datasets.ImageFolder(root + 'dataset/BSDS200', transform=test_bsds_transforms)
-    test_set5 = torchvision.datasets.ImageFolder(root + 'dataset/set5', transform=test_set5_transforms)
-    test_set14 = torchvision.datasets.ImageFolder(root + 'dataset/set14', transform=test_set14_transforms)
-    compare = torchvision.datasets.ImageFolder(root + 'dataset/set14', transform=test_compare_transforms)
-
-    test_loader_bsds = DataLoader(test_bsds, batch_size=1, shuffle=True, **kwopt, drop_last=False)
-    test_loader_set5 = DataLoader(test_set5, batch_size=1, shuffle=True, **kwopt, drop_last=False)
-    test_loader_set14 = DataLoader(test_set14, batch_size=1, shuffle=True, **kwopt, drop_last=False)
-    test_loader_compare = DataLoader(compare, batch_size=1, shuffle=True, **kwopt, drop_last=False)
-
-    if is_test:
-        return None, test_loader_bsds, test_loader_set5, test_loader_set14, test_loader_compare
-
-    trn_loader = DataLoader(trn_dataset, batch_size=args.batch_size, shuffle=True, **kwopt,
-                            drop_last=False)
-
-    return trn_loader, test_loader_bsds, test_loader_set5, test_loader_set14
->>>>>>> facfa192f412a4c562019f1a1253961ac48661e2
