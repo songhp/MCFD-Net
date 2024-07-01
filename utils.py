d7ac016e751a03c39043fd4df47f82621214b69f
jingyaogong <62287848+jingyaogong@users.noreply.github.com>
2024-07-01 23:59:01 +0800

    AAAI 2024

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from torch.autograd import Variable
 from math import *
 import torch.nn.functional as F
 import os
+from skimage.metrics import structural_similarity as ssim_count
 
 USE_MULTI_GPU = False
 use_half_precision_flag = False
@@ -47,75 +48,5 @@ def create_window(window_size, channel=1):
     window = _2D_window.expand(channel, 1, window_size, window_size).contiguous()
     return window
 
-
-def ssim(img1, img2, window_size=11, window=None, size_average=True, full=False, val_range=None):
-    if val_range is None:
-        if torch.max(img1) > 128:
-            max_val = 255
-        else:
-            max_val = 1
-
-        if torch.min(img1) < -0.5:
-            min_val = -1
-        else:
-            min_val = 0
-        L = max_val - min_val
-    else:
-        L = val_range
-
-    padd = 0
-    (_, channel, height, width) = img1.size()
-    if window is None:
-        real_size = min(window_size, height, width)
-        window = create_window(real_size, channel=channel).to(test_device)
-
-    mu1 = F.conv2d(img1, window, padding=padd, groups=channel)
-    mu2 = F.conv2d(img2, window, padding=padd, groups=channel)
-
-    mu1_sq = mu1.pow(2)
-    mu2_sq = mu2.pow(2)
-    mu1_mu2 = mu1 * mu2
-    sigma1_sq = F.conv2d(img1 * img1, window, padding=padd, groups=channel) - mu1_sq
-    sigma2_sq = F.conv2d(img2 * img2, window, padding=padd, groups=channel) - mu2_sq
-    sigma12 = F.conv2d(img1 * img2, window, padding=padd, groups=channel) - mu1_mu2
-
-    C1 = (0.01 * L) ** 2
-    C2 = (0.03 * L) ** 2
-
-    v1 = 2.0 * sigma12 + C2
-    v2 = sigma1_sq + sigma2_sq + C2
-    cs = torch.mean(v1 / v2)
-
-    ssim_map = ((2 * mu1_mu2 + C1) * v1) / ((mu1_sq + mu2_sq + C1) * v2)
-
-    if size_average:
-        ret = ssim_map.mean()
-    else:
-        ret = ssim_map.mean(1).mean(1).mean(1)
-
-    if full:
-        return ret, cs
-    return ret
-
-
-class SSIM(torch.nn.Module):
-    def __init__(self, window_size=11, size_average=True, val_range=None):
-        super(SSIM, self).__init__()
-        self.window_size = window_size
-        self.size_average = size_average
-        self.val_range = val_range
-
-        self.channel = 1
-        self.window = create_window(window_size).to(test_device)
-
-    def forward(self, img1, img2):
-        (_, channel, _, _) = img1.size()
-
-        if channel == self.channel and self.window.dtype == img1.dtype:
-            window = self.window
-        else:
-            window = create_window(self.window_size, channel).to(img1.device).type(img1.dtype)
-            self.window = window
-            self.channel = channel
-
-        return ssim(img1, img2, window=window, window_size=self.window_size, size_average=self.size_average)
+def ssim(img1, img2):
+    return ssim_count(img1.squeeze().cpu().numpy(), img2.squeeze().cpu().numpy(), data_range=1)
